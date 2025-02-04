ec18d0c100b845046be9c41de7bb264d384c3b72
bulacio nicolas <93942151+nicolasbulacio@users.noreply.github.com>
2025-02-04 15:15:21 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 
 
 //Constructor de Alumnos
-function Alumno (nombre,edad,nota1,nota2,nota3) {
+function Alumno (nombre,nota1,nota2,nota3) {
 
     this.nombre=nombre,
-    this.edad=edad,
+    
     this.nota1=nota1,
     this.nota2=nota2,
     this.nota3=nota3,
     this.promedio=function(){return((nota1+nota2+nota3)/3)}
 }
 
-let alumno1 = new Alumno("Alejandro",25,8,9,9)
-let alumno2= new Alumno("Pedro",19,4,3,2)
-let alumno3 = new Alumno("Nicolas",32,5,9,10)
-let alumno4 = new Alumno("Juan",30,2,5,6)
-let alumno5 = new Alumno("Gonzalo",27,5,7,5)
+let alumno1 = new Alumno("Alejandro",8,9,9)
+let alumno2= new Alumno("Pedro",4,3,2)
+let alumno3 = new Alumno("Nicolas",5,9,10)
+let alumno4 = new Alumno("Juan",2,5,6)
+let alumno5 = new Alumno("Gonzalo",5,7,5)
 
 //Array de alumnos
 
@@ -23,15 +23,11 @@ let cursado=[alumno1,alumno2,alumno3,alumno4,alumno5]
 
 //Local storage
 
-/*if(localStorage.getItem("alumnos")){
-    cursado=JSON.parse(localStorage.getItem("alumnos"))
-}else{
-    cursado=cursado
-}*/
+
 
 if (localStorage.getItem("alumnos")) {
     cursado = JSON.parse(localStorage.getItem("alumnos")).map(alumnoData => {
-        return new Alumno(alumnoData.nombre, alumnoData.edad, alumnoData.nota1, alumnoData.nota2, alumnoData.nota3);
+        return new Alumno(alumnoData.nombre, alumnoData.nota1, alumnoData.nota2, alumnoData.nota3);
     });
 } else {
     cursado = cursado;
@@ -40,12 +36,12 @@ if (localStorage.getItem("alumnos")) {
 //Agregar alumnos al Array
 function agregarAlumnos(){
     let nombre=prompt("Ingrese el nombre del alumno")
-    let edad=prompt("Ingrese la edad del alumno")
+    
     let nota1=prompt("Ingrese la nota del primer trimestre")
     let nota2=prompt("Ingrese la nota del segundo trimestre")
     let nota3=prompt("Ingrese la nota del tercer trimestre")
 
-    let alumno=new Alumno (nombre,edad,nota1,nota2,nota3)
+    let alumno=new Alumno (nombre,nota1,nota2,nota3)
     cursado.push(alumno)
     localStorage.setItem("alumnos",JSON.stringify(cursado))
 
